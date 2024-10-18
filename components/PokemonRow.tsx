b4edcdd3c36142b471aa03f3377fd641987e0d89
Txusif <txusif@gmail.com>
2024-10-18 15:36:39 +0530

    added Image instead of img

--- components/PokemonRow.tsx
+++ components/PokemonRow.tsx
@@ -1,10 +1,19 @@
 import React from "react";
 import { Pokemon } from "./types";
+import Image from "next/image";
 
 export const PokemonRow: React.FC<{ pokemon: Pokemon }> = ({ pokemon }) => {
   return (
     <div className="flex items-center space-x-4 p-2 border-b border-gray-700 last:border-b-0">
-      <img src={pokemon.sprite} alt={pokemon.name} className="w-16 h-16" />
+      <div className="relative w-16 h-16">
+        <Image
+          src={pokemon.sprite}
+          alt={pokemon.name}
+          layout="fill"
+          quality={100}
+          className="object-contain"
+        />
+      </div>
       <div>
         <p className="font-bold text-white">{pokemon.name}</p>
         <p className="text-gray-300">Id: {pokemon.id}</p>
